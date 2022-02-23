3565dd001921ed06b5d332fd052582c5e1fd4342
natnatjr <ejnatnat@yahoo.com>
2022-02-23 10:13:59 -0500

    Added a class "TestUserProfile" as main() class where if user creates a user profile, program would print a message "user profile has been created"

---
+++ src/midtermexam_versiona_extensioncode/TestUserprofile.java
@@ -0,0 +1,13 @@
+package midtermexam_versiona_extensioncode;
+/**
+ * @author Rich Smith at ZenOfProgramming.com
+ */
+
+public class TestUserprofile
+{
+   public static void main (String[] args)
+   {
+      UserProfile input = new UserProfile("natnatjr", "Comedy");
+
+   }
+}
--- src/midtermexam_versiona_extensioncode/UserProfile.java
+++ src/midtermexam_versiona_extensioncode/UserProfile.java
@@ -11,58 +11,67 @@ package midtermexam_versiona_extensioncode;
  * genres array. This code is to be used for SYST 17796 midterm exam.
  * Students are reminded to add themselves as modifiers when editing the
  * code.
+ *
  * @author dancye, 2019
  */
-public class UserProfile 
+public class UserProfile
 {
-    
-    private String userID;//the userID
-    private String genre;// the user's preferred genre of movie
-    private String[] genres = {"Comedy", "Drama", "Action", "Mystery"};//available genres of movies
-    
-    /**
-     * A constructor that takes in the userID and the favourite genre
-     * @param givenID the ID to assign to this user
-     * @param givenGenre the users favourite genre
-     */
-    public UserProfile(String givenID, String givenGenre)
-    {
-        userID = givenID;
-        genre= givenGenre;
-    }
 
-    /**
-     * A getter for the userID
-     * @return the userID
-     */
-    public String getUserID() {
-        return userID;
-    }
+   private String userID;//the userID
+   private String genre;// the user's preferred genre of movie
+   private String[] genres = {"Comedy", "Drama", "Action", "Mystery"};//available genres of movies
 
-    /**
-     * @param userID the userID to set
-     */
-    public void setUserID(String userID) {
-        this.userID = userID;
-    }
+   /**
+    * A constructor that takes in the userID and the favourite genre
+    *
+    * @param givenID the ID to assign to this user
+    * @param givenGenre the users favourite genre
+    */
+   public UserProfile (String givenID, String givenGenre)
+   {
+      userID = givenID;
+      genre = givenGenre;
+      System.out.println("user profile has been created");
+   }
 
-    /**
-     * A getter for the genre
-     * @return the genre
-     */
-    public String getGenre() {
-        return genre;
-    }
+   /**
+    * A getter for the userID
+    *
+    * @return the userID
+    */
+   public String getUserID ()
+   {
+      return userID;
+   }
 
-    /**
-     * @param genre the genre to set
-     */
-    public void setGenre(String genre) {
-        this.genre = genre;
-    }
-    
-    
-    
-    
-    
+   /**
+    * @param userID the userID to set
+    */
+   public void setUserID (String userID)
+   {
+      this.userID = userID;
+   }
+
+   /**
+    * A getter for the genre
+    *
+    * @return the genre
+    */
+   public String getGenre ()
+   {
+      return genre;
+   }
+
+   /**
+    * @param genre the genre to set
+    */
+   public void setGenre (String genre)
+   {
+      this.genre = genre;
+   }
+
+
+
+
+
 }//end class
